94403b0c5c880cefccf0bc33c512e83fe63c3ffd
Kaan Biryol <dev.biryolkaan@gmail.com>
2026-03-29 01:16:15 +0100

    chore: add tuist test fixture from examples repo

---
+++ test_fixture/FrameworkTests/FrameworkTests.swift
@@ -0,0 +1,19 @@
+import Foundation
+import XCTest
+@testable import Framework
+
+final class FrameworkTests: XCTestCase {
+    var subject = FrameworkClass()
+
+    func testExample() {
+        print("Framework tests example")
+    }
+}
+
+final class FrameworkAnotherTests: XCTestCase {
+    var subject = FrameworkClass()
+
+    func testAnotherExample() {
+        print("Framework tests another example")
+    }
+}
